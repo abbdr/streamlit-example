3ec5d23f460413eeea687b271965565212d77ecf
abbdr <142697206+abbdr@users.noreply.github.com>
2024-04-09 17:24:23 +0700

    Update input.py

--- test/pages/input.py
+++ test/pages/input.py
@@ -1,15 +1,9 @@
 import streamlit as st
 
-if 'input' not in st.session_state:
-    st.session_state['input'] = 0
-  
-st.session_state['input'] = st.text_input('')
-query = st.session_state['input']
+query = ''
+def callback(string):
+    query = string
 
-if st.button('Save Filters'):
-        st.session_state['input'] = query
-
-if st.button('Clear page Filters'):
-        st.session_state['input'] = 0 # or default value
+input_text = st.text_area("Enter a text", key="input_text", on_change=callback)
 
 'query : ',query
