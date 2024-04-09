e5a7e75533168d8b8bcb7ba5469155234664501f
abbdr <142697206+abbdr@users.noreply.github.com>
2024-04-09 17:30:38 +0700

    Update input.py

--- test/pages/input.py
+++ test/pages/input.py
@@ -1,14 +1,6 @@
 import streamlit as st
 
+def proc():
+    st.write(st.session_state.text_key)
 
-@st.cache_data
-def input():
-    data = st.text_input('')
-    return data
-    
-query = input()
-
-if st.button('reset'):
-    query = 0
-
-'query : ',query
+st.text_area('enter text', on_change=proc, key='text_key')
