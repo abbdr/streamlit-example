38f37852276bc555b3c7d7c292a9dd4cb29a436e
abbdr <142697206+abbdr@users.noreply.github.com>
2024-04-09 19:23:00 +0700

    Update input.py

--- test/pages/input.py
+++ test/pages/input.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 input_user = st.text_input("Masukkan teks Anda di sini:")
-    if st.button("Simpan"):
-        st.session_state['input_user'] = input_user
-        st.success("Input berhasil disimpan!")
+if st.button("Simpan"):
+    st.session_state['input_user'] = input_user
+    st.success("Input berhasil disimpan!")
 
