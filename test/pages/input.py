e45c01a28f239c97edd6488366228296cd7c6b49
abbdr <142697206+abbdr@users.noreply.github.com>
2024-04-09 17:16:27 +0700

    Update input.py

--- test/pages/input.py
+++ test/pages/input.py
@@ -3,7 +3,9 @@ import streamlit as st
 if 'input' not in st.session_state:
     st.session_state['input'] = 0
   
-st.session_state['input'] = st.text_input()
+
+query = st.text_input()
+st.session_state['input'] = query
 query = st.session_state['input']
 
 'query : ',query
