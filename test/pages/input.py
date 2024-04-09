73d49f834c41928d9bea81801bb65e9720996137
abbdr <142697206+abbdr@users.noreply.github.com>
2024-04-09 17:53:09 +0700

    Update input.py

--- test/pages/input.py
+++ test/pages/input.py
@@ -1,9 +1,4 @@
 import streamlit as st
 
-query = ''
-def proc():
-    query = st.cache.text_key
-    st.write(st.cache.text_key)
-
-st.text_area('enter text', on_change=proc, key='text_key')
+query = 'aaaaaaaaaaaaaappppppppppppppppppppppaaaaaaaaaaaaaaaaaaaaappppppppppppppppp'
 query
