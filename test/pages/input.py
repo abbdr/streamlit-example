3dcd02e774f8d9dd1a00e88ee1b589a32773f341
abbdr <142697206+abbdr@users.noreply.github.com>
2024-04-09 17:20:14 +0700

    Update input.py

--- test/pages/input.py
+++ test/pages/input.py
@@ -6,4 +6,10 @@ if 'input' not in st.session_state:
 st.session_state['input'] = st.text_input('')
 query = st.session_state['input']
 
+if st.button('Save Filters'):
+        st.session_state['user_select_value'] = user_select_value
+
+if st.button('Clear page Filters'):
+        st.session_state['user_select_value'] = 0 # or default value
+
 'query : ',query
