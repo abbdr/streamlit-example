9134fdb9136f8fdfcd91a6e2d7413ffe90f107c5
abbdr <142697206+abbdr@users.noreply.github.com>
2024-04-09 17:18:10 +0700

    Update input.py

--- test/pages/input.py
+++ test/pages/input.py
@@ -4,7 +4,7 @@ if 'input' not in st.session_state:
     st.session_state['input'] = 0
   
 
-query = st.text_input()
+query = st.text_input('')
 st.session_state['input'] = query
 query = st.session_state['input']
 
