70f70b44713099b1202ea2bd86d49ad65514652c
abbdr <142697206+abbdr@users.noreply.github.com>
2024-04-09 17:15:04 +0700

    Update input.py

--- test/pages/input.py
+++ test/pages/input.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-query = st.text_input('ketik query')
+if 'input' not in st.session_state:
+    st.session_state['input'] = 0
+  
+st.session_state['input'] = st.text_input()
+query = st.session_state['input']
+
 'query : ',query
