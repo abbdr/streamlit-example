1ab7d5f1a1792a169d5e47a82990c1a5d5430dc8
abbdr <142697206+abbdr@users.noreply.github.com>
2024-04-11 21:00:15 +0700

    Update training.py

--- test/pages/training.py
+++ test/pages/training.py
@@ -90,7 +90,7 @@ data_pre['cleaned']
 
 if 'data' in st.session_state:
     input = st.session_state['data']
-    dataku = data_pre['cleaned'].tolist()
+    dataku = data_pre['cleaned'].tolist().append(input)
     dataku
 
 
