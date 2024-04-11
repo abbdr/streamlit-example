d92f07787bb43a86f92d81ba0336e99e95b2bec4
abbdr <142697206+abbdr@users.noreply.github.com>
2024-04-11 21:19:20 +0700

    Update training.py

--- test/pages/training.py
+++ test/pages/training.py
@@ -88,6 +88,7 @@ data_pre['cleaned'] = stem()
 '## \'Clean\' Data'
 data_pre['cleaned']
 
+dataku = ''
 if 'data' in st.session_state:
     input = st.session_state['data']
     dataku = data_pre['cleaned'].tolist()
