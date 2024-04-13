48678b926b945b845341f0367d94557671657241
abbdr <142697206+abbdr@users.noreply.github.com>
2024-04-13 11:25:49 +0700

    Update training.py

--- test/pages/training.py
+++ test/pages/training.py
@@ -14,7 +14,9 @@ with st.spinner('Getting Dataset...'):
     df = pd.read_csv('dataset_tweet_sentimen_tayangan_tv.csv')
     st.session_state['sentiment'] = df['Sentiment']
     st.session_state['id'] = df['Id']
-    df['Id']
+    a = st.session_state['id']
+    a
+    df
 
 data_pre = pd.DataFrame(df['Text Tweet'])
 '## Sentences Data'
