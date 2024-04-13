471d1d550d8fd35b2607bc4b1736240b22524f74
abbdr <142697206+abbdr@users.noreply.github.com>
2024-04-13 17:34:44 +0700

    Update input.py

--- test/pages/input.py
+++ test/pages/input.py
@@ -49,7 +49,7 @@ input = st.text_input("Masukkan teks Anda di sini:")
 k = st.number_input("Masukkan nilai k di sini (ganjil):",3,201,value=3,step=2)
 k = k+1 if k%2 else k
 if st.button('simpan'):
-    st.session_state['input_df'] = pd.DataFrame(input)
+    st.session_state['input_df'] = pd.DataFrame([input])
     input_user = input
     st.session_state['k'] = k
     query = [input_user]
