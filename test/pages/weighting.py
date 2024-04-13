e1ddea75c18d2c4ccff348e5479693d4014375b7
abbdr <142697206+abbdr@users.noreply.github.com>
2024-04-13 17:32:15 +0700

    Update weighting.py

--- test/pages/weighting.py
+++ test/pages/weighting.py
@@ -108,7 +108,7 @@ if 'dataku' in st.session_state:
       c.append(i/b)
       a += 1
 
-  st.write('input : ', st.session_state['data'][0])
+  st.write('input : ', st.session_state['input_df'])
   c3 = sorted(zip(c, st.session_state['id'], st.session_state['data_awal'], st.session_state['sentiment']),  reverse=True)
   c3 = pd.DataFrame(c3)
   c3.rename(columns = {0:'Relevancy', 1:'Id', 2:'terms', 3:'Sentiment'}, inplace = True)
