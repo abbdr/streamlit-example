98e2a2d918e258bf453db2a310910a42ebd5e6dd
abbdr <142697206+abbdr@users.noreply.github.com>
2024-04-13 17:13:06 +0700

    Update input.py

--- test/pages/input.py
+++ test/pages/input.py
@@ -46,8 +46,8 @@ query = ''
 data_pre = ''
 data = ''
 input = st.text_input("Masukkan teks Anda di sini:")
-k = st.number_input("Masukkan nilai k di sini (ganjil):",3,201,value=3)
-k = k+1 if k%2 else k
+k = st.number_input("Masukkan nilai k di sini (ganjil):",3,201,value=3,step=1)
+# k = k+1 if k%2 else k
 if st.button('simpan'):
     input_user = input
     st.session_state['k'] = k
