e849e49c9b684a3a8ebcdaf2f6d5ea6c7b3c4a30
Rozag <www.rozag@gmail.com>
2024-04-14 14:23:24 +0700

    Update D_output.py

--- test/pages/D_output.py
+++ test/pages/D_output.py
@@ -4,6 +4,6 @@ if 'c' in st.session_state:
   st.write('input : ', st.session_state['input_df'])
   st.write('k : ',st.session_state['k'])
   ck = sorted(zip(st.session_state['c'], st.session_state['id'], st.session_state['data_awal'], st.session_state['sentiment']),  reverse=True)[:st.session_state['k']]
-  ck = pd.DataFrame(c3)
+  ck = pd.DataFrame(ck)
   ck.rename(columns = {0:'Relevancy', 1:'Id', 2:'terms', 3:'Sentiment'}, inplace = True)
   ck
